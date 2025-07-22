39e0967d61049762395712945d48340cf8a84c1b
MANIKANTA-KURAKULA <105640474+MANIKANTA-KURAKULA@users.noreply.github.com>
2025-07-21 20:43:37 -0500

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -7,7 +7,7 @@ unzip awscliv2.zip
 sudo ./aws/install
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo +x kubectl
+sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
